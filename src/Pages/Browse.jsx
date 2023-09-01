07aac2cbf601aeda7cf9731a26bbb5aeb228a67e
sardonian <plutofeb115@gmail.com>
2023-09-01 14:23:55 +0530

    ADD: scroll button

--- src/Pages/Browse.jsx
+++ src/Pages/Browse.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Axios from "axios";
 
-import { East } from "@mui/icons-material";
+import { ChevronLeft, ChevronRight, East } from "@mui/icons-material";
 
 import Carousel from "../Components/Carousel";
 import Card from "../Components/Card";
@@ -62,7 +62,7 @@ function Movies({ setLoading, setNavIndx, navIndx, type, adult }) {
 
   useEffect(() => {
     type === "movie" ? setNavIndx(0) : setNavIndx(1);
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    // window.scrollTo({ top: 0, behavior: "smooth" });
     setLoading(true);
     function getCarouselData() {
       Axios.get(
@@ -172,19 +172,56 @@ function Movies({ setLoading, setNavIndx, navIndx, type, adult }) {
                   <East fontSize="large" />
                 </span>
               </p>
-              <div className="flex gap-3 overflow-x-auto py-3 pl-8 mb-4 overflow-y-hidden items-center whitespace-nowrap hide-scroll group cards">
-                {element.data.slice(0, 15).map((item, indx) => {
-                  return <Card item={item} key={indx} type={type} />;
-                })}
-                <div className="flex flex-col items-center gap-4 ml-12">
-                  <Link
-                    onClick={() => window.scrollTo({ top: 0 })}
-                    to={element.to}
-                    className="bg-neutral-800 rounded-full p-4 lg:mx-28 hover:lg:scale-110 duration-300"
+              <div className="relative">
+                <div
+                  className="flex gap-3 overflow-x-auto py-3 pl-8 mb-4 overflow-y-hidden items-center whitespace-nowrap hide-scroll group cards scroll-smooth"
+                  id={`cards-${index}`}
+                  onScroll={() => {
+                    const element = document.getElementById(`cards-${index}`);
+                    if (Math.round(element.scrollLeft) === 0)
+                      document.getElementById("prev").classList.remove("show");
+                    else document.getElementById("prev").classList.add("show");
+                    if (
+                      Math.round(element.scrollWidth - element.clientWidth) ===
+                      Math.round(element.scrollLeft)
+                    )
+                      document.getElementById("next").classList.remove("show");
+                    else document.getElementById("next").classList.add("show");
+                  }}
+                >
+                  {element.data.slice(0, 15).map((item, indx) => {
+                    return <Card item={item} key={indx} type={type} />;
+                  })}
+                  <div className="flex flex-col items-center gap-4 ml-12">
+                    <Link
+                      onClick={() => window.scrollTo({ top: 0 })}
+                      to={element.to}
+                      className="bg-neutral-800 rounded-full p-4 lg:mx-28 hover:lg:scale-110 duration-300"
+                    >
+                      <East fontSize="large" />
+                    </Link>
+                    <p className="text-xl">View more</p>
+                  </div>
+                  <div
+                    id="prev"
+                    className="lg:flex justify-center text-5xl items-center absolute left-0 z-20 h-[75%] prev hidden"
+                    onClick={() => {
+                      const element = document.getElementById(`cards-${index}`);
+                      element.scrollLeft -= 1500;
+                    }}
                   >
-                    <East fontSize="large" />
-                  </Link>
-                  <p className="text-xl">View more</p>
+                    <ChevronLeft fontSize="inherit" />
+                  </div>
+                  <div
+                    id="next"
+                    className="lg:flex justify-center text-5xl items-center absolute right-0 z-20 h-[75%] next hidden"
+                    onClick={() => {
+                      const element = document.getElementById(`cards-${index}`);
+                      element.scrollLeft += 1500;
+                    }}
+                  >
+                    <ChevronRight fontSize="inherit" />
+                  </div>
                 </div>
               </div>
             </div>
