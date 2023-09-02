d87b7b8630a29527ccbc6cdb01977febe2f85ee7
paturikaustubh <paturikaustubh@users.noreply.github.com>
2023-09-02 06:39:45 +0000

    deploy: 77ee89e82852e8aae6b1231b6b37bf4ebc7a5674

---
+++ assets/Test-2087ba50.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-a95e6418.js";function r(){return t.jsx("div",{children:"Test"})}export{r as default};
--- assets/Test-5e89b895.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-6a882c0b.js";function r(){return t.jsx("div",{children:"Test"})}export{r as default};
